a4b5756cf95e359d81351c0bcb0260971b0ba91e
bipin24746 <sainjubipin247460@gmail.com>
2025-01-28 16:27:04 +0545

    add

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:todo_with_dependency_injection/core/di.dart';
 
 void main() {
-  setupDependencies();
+  setupDependencies(); // Set up Dependency Injection
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'To-Do App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
       home: const TodoScreen(),
     );
   }
 }
-
 class TodoScreen extends StatelessWidget {
   const TodoScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final todoService = serviceLocator<TodoService>(); //retrieves the same instance of TodoServices that we register earlier
+    // Retrieve the TodoService instance
+    final todoService = serviceLocator<TodoService>();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('To-Do List'),
       ),
       body: Center(
-        child: Text(todoService.fetchData()),
+        child: Text(todoService.fetchData()), // Use the service method
       ),
     );
   }
